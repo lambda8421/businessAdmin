08c293c7c69ac932db4a409d3309be9ff184083e
lambda8421 <lambda8421@163.com>
2018-03-18 00:34:37 -0500

    test branch

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ test(a)
 ReactDOM.render(
     <div>
         <i className="fa fa-address-book"></i>
-        <h1>Hello, world!</h1>
+        <h1>Hello, worldffff!</h1>
     </div>
     ,
 document.getElementById('root')
